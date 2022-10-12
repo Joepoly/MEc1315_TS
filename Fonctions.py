5f589f2a3e4325f3d4d7ff43e2709b1e45d4410a
Joepoly <joel.archambault04@gmail.com>
2022-10-12 18:25:22 -0400

    Update Fonctions.py
    J'ai trouvé les erreurs dans repetiton circulaire et j'ai modifié certain trucs dans d'autres fonction en les testant. Donc j'ai remis tout de mon fichier source !

--- Fonctions.py
+++ Fonctions.py
@@ -40,7 +40,7 @@ def affinite (objet,facteur_x,facteur_y,facteur_z):
     v[:,1]=v[:,1]*facteur_y
     v[:,2]=v[:,2]*facteur_z
     n=CalculNormal(f,v)
-    return f,v,n  
+    return f,v,n    
     
 def centre_sur_axe(objet,axe): 
     if axe =='x':
@@ -60,63 +60,79 @@ def centre_sur_axe(objet,axe):
         z_min=min(v[:,2])
         v=v-np.array([0,0,z_min])
         return f,n,v
-   
+
 def centre_000(objet):
     f,v,n=np.array(objet[0]),np.array(objet[1]),np.array(objet[2])
-    x_centre= max(v[:,0])-min(v[:,0])/2
-    y_centre= max(v[:,1])-min(v[:,1])/2
-    z_centre= max(v[:,2])-min(v[:,2])/2
+    x_centre= (max(v[:,0])-min(v[:,0]))/2
+    y_centre= (max(v[:,1])-min(v[:,1]))/2
+    z_centre= (max(v[:,2])-min(v[:,2]))/2
     v=v - np.array([x_centre,y_centre,z_centre])
     return f,v,n
- 
+    
+        
+    
 def fusion(*args):
-    fi,vi,ni=[],[],[]
+    F,V,N=[],[],[]
     nv=0
     
-    for j in range (len(args)):
-        fi.append(args[j][0]+nv)
-        vi.append(args[j][1])
-        ni.append(args[j][2])
+    for i in range(len(args)):
+        F.append(args[i][0] + nv)
+        V.append(args[i][1])
+        N.append(args[i][2])
+
+        nv= nv+len(V[i])
         
-        nv= nv+len(vi)
+        f=np.vstack(F)
+        v=np.vstack(V)
+        n=np.vstack(N)
         
-        f=np.vstack(fi)
-        v=np.vstack(vi)
-        n=np.vstack(ni)
-        
-        return f,v,n
-    
-    
-def repetition_circulaire(objet,nb_repetition,axe_rotation,position_reference):
+    return f,v,n
+
+
+def repetition_circulaire(objet,nb_repetition,axe_rotation,offset):
     f1,v1,n1=np.array(objet[0]),np.array(objet[1]),np.array(objet[2])
     nv=len(v1)
     angle= 2*np.pi/nb_repetition
-    f,v,n=np.empty((0,3)),np.empty((0,3)),np.empty((0,3))
     
-    v1=v1[:,0]+position_reference[0]
-    v1=v1[:,1]+position_reference[1]
-    v1=v1[:,2]+position_reference[2]
+ 
     
     if axe_rotation=='x':
+        v1[:,0]=v1[:,0]+offset
+        v1[:,1]=v1[:,1]+offset
+       
+        
+        f,v,n = np.empty([0,3]), np.empty([0,3]), np.empty([0,3])
+        
         for i in range (nb_repetition):
             stack = nv*i
-            f=np.vstack(f,f1+stack)
-            v=np.vstack(v,np.dot(v1,Rx(j*angle)))
-            n=np.vstack(n,np.dot(n1,Rx(j*angle)))
-            return f,v,n
+            f=np.vstack((f,f1+stack))
+            v=np.vstack((v,np.dot(v1,Rx(i*angle))))
+            n=np.vstack((n,np.dot(n1,Rx(i*angle))))
+        return f,v,n
         
     elif axe_rotation=='y':
+        v1[:,0]=v1[:,0]+offset
+        v1[:,1]=v1[:,1]+offset
+        
+        f,v,n = np.empty([0,3]), np.empty([0,3]), np.empty([0,3])
+        
         for i in range (nb_repetition):
             stack = nv*i
-            f=np.vstack(f,f1+stack)
-            v=np.vstack(v,np.dot(v1,Ry(j*angle)))
-            n=np.vstack(n,np.dot(n1,Ry(j*angle)))
-            return f,v,n
+            f=np.vstack((f,f1+stack))
+            v=np.vstack((v,np.dot(v1,Ry(i*angle))))
+            n=np.vstack((n,np.dot(n1,Ry(i*angle))))
+        return f,v,n
         
     elif axe_rotation=='z':
+        v1[:,0]=v1[:,0]+offset
+        v1[:,1]=v1[:,1]+offset
+        
+        f,v,n = np.empty([0,3]), np.empty([0,3]), np.empty([0,3])
+        
         for i in range (nb_repetition):
             stack = nv*i
-            f=np.vstack(f,f1+stack)
-            v=np.vstack(v,np.dot(v1,Rz(j*angle)))
-            n=np.vstack(n,np.dot(n1,Rz(j*angle)))
-            return f,v,n
+            f=np.vstack((f,f1+stack))
+            v=np.vstack((v,np.dot(v1,Rz(i*angle))))
+            n=np.vstack((n,np.dot(n1,Rz(i*angle))))
+        return f,v,n
+        
