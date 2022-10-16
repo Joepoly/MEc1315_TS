ee28905a5dc423641cff769873e9c97ea52382b0
minabouks <78928104+minabouks@users.noreply.github.com>
2022-10-16 16:34:48 -0400

    Joel CHANGEMENT

--- Joel_scene.py
+++ Joel_scene.py
@@ -17,38 +17,38 @@ import random
 cube=LireSTL('Cube.stl')
 
 terrain = cube
-terrain = affinite(terrain, 2500, 2500, 20)
-terrain = centre_000(terrain)
-terrain = translation(terrain, 0, 0, 10)
+terrain = Affinite(terrain, 2500, 2500, 20)
+terrain = Centre_000(terrain)
+terrain = Translation(terrain, 0, 0, 10)
 #=======================Drapeaux de coin=========================#
 triangle =LireSTL('Triangle.stl')
 cylindre =LireSTL('Cylindre.stl')
 
 tige=cylindre
-tige=affinite(tige, 30, 30, 300)
-tige=translation(tige, -1230, -1230, 0)
-tige=repetition_rectangulaire(tige, 1, 'y', 2450)
+tige=Affinite(tige, 30, 30, 300)
+tige=Translation(tige, -1230, -1230, 0)
+tige=Repetition_Rectangulaire(tige, 2, 'y', 4900)
 
 drapeau=triangle
-drapeau=affinite(drapeau, 50, 50, 30)
-drapeau=rotation_x_y_z(drapeau, 'y', 90)
-drapeau=rotation_x_y_z(drapeau, 'z', -90)
-drapeau=translation(drapeau, -1215, -1215, 300)
-drapeau=repetition_rectangulaire(drapeau, 1, 'y', 2450)
+drapeau=Affinite(drapeau, 50, 50, 30)
+drapeau=Rotation_x_y_z(drapeau, 'y', 90)
+drapeau=Rotation_x_y_z(drapeau, 'z', -90)
+drapeau=Translation(drapeau, -1215, -1215, 300)
+drapeau=Repetition_Rectangulaire(drapeau, 2, 'y', 4900)
 
 #=======================TEEMO=========================#
 perso=LireSTL('Teemo_nobase.stl')
 teemo=perso
-teemo=homotetie(teemo, 12)
-teemo=translation(teemo, 0, 200, 20)
-teemo=rotation_x_y_z(teemo, 'z', 290)
+teemo=Homotetie(teemo, 12)
+teemo=Translation(teemo, 0, 200, 20)
+teemo=Rotation_x_y_z(teemo, 'z', 290)
 
 #=======================Ballon=========================#
 ico=LireSTL('icosahedron.stl')
 ballon=ico
-ballon=homotetie(ballon, 60)
-ballon=translation(ballon, -120, -300, 20)
-ballon=repetition_circulaire(ballon,random.randint(1, 10) , 'z', -10, -1)
+ballon=Homotetie(ballon, 60)
+ballon=Translation(ballon, -120, -300, 20)
+ballon=Repetition_Circulaire(ballon,random.randint(1, 10) , 'z', -10, 20,0)
 
 
 
