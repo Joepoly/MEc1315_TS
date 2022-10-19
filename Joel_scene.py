ba8820eaebf967a33a0d0d04f8748cbe3be7f1d6
markec3 <115379479+markec3@users.noreply.github.com>
2022-10-19 19:53:23 -0400

    Ajout Gazon

--- Joel_scene.py
+++ Joel_scene.py
@@ -186,10 +186,31 @@ man = Rotation_x_y_z(man,"x",180)
 man = Rotation_x_y_z(man,"z",90)
 man = Translation(man,-200,-350,450)
 
+#====================Gazon==========================
+
+gazon1 = LireSTL('icosahedron.stl')
+gazon1 = Affinite (gazon1,20,0,30)
+gazon1 = Rotation_x_y_z(gazon1, "x", 45)
+gazon1 = Repetition_Rectangulaire(gazon1, 15, [500,0,0])
+gazon1 = Repetition_Rectangulaire(gazon1, 100, [0,2990,0])
+gazon1 = Translation(gazon1,0,10,0)
+
+gazon2 = LireSTL('icosahedron.stl')
+gazon2 = Affinite (gazon2,20,0,30)
+gazon2 = Rotation_x_y_z(gazon2, "x", -45)
+gazon2 = Repetition_Rectangulaire(gazon2, 15, [500,0,0])
+gazon2 = Repetition_Rectangulaire(gazon2, 100, [0,2990,0])
+gazon2 = Translation(gazon2,500,-10,0)
+
+gazon3 = Fusion(gazon1,gazon2)
+gazon3 = Repetition_Rectangulaire(gazon3, 2, [2000,0,0])
+
+gazon = Fusion(gazon1,gazon2,gazon3)
+gazon = Translation(gazon,-1500,-1500,10)
 
 #================Fusion+export======================#
  
-terrain_f=Fusion(terrain,tige,drapeau,teemo,ballon,panneau,estrade, bonhomme, but, camille, fiora, man)
+terrain_f=Fusion(terrain,tige,drapeau,teemo,ballon,panneau,estrade, bonhomme, but, camille, fiora, man,gazon)
 
 
 
